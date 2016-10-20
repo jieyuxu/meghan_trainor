7550623f310939ff17f9175c81840a539327fdd1
jieyuxu <jieyu.stuy@gmail.com>
2016-10-20 12:06:29 -0400

    commented out useless functionm

--- get_averages.py
+++ get_averages.py
@@ -4,22 +4,22 @@ import sqlite3
 db = sqlite3.connect("stud_info.db")
 cur = db.cursor()
 
-def getGrade(num):
-	ret = "The grades of the student with id # " + str(num) + " are: "
-	query = "SELECT grade FROM courses WHERE %s == courses.id"%(num)
-	sel = cur.execute(query)
-	for record in sel:
-		ret += str(record).strip(')(,') + ", "
+# def getGrade(num):
+# 	ret = "The grades of the student with id # " + str(num) + " are: "
+# 	query = "SELECT grade FROM courses WHERE %s == courses.id"%(num)
+# 	sel = cur.execute(query)
+# 	for record in sel:
+# 		ret += str(record).strip(')(,') + ", "
 	
-	return ret[:-2]
+# 	return ret[:-2]
 
 def compavg(num):
 	tot = 0.0
 	n = 0.0
 	query = "SELECT grade FROM courses, students WHERE %s == courses.id"%(num)
-	sel = cur.execute(query)
-	for record in sel:
-		tot += float(str(record).strip(')(,')) 
+	sel = (cur.execute(query)).fetchall()
+	for item in sel:
+		tot += item[0]
 		n+=1
 	return tot/n
 
@@ -34,9 +34,6 @@ def prinfo():
 	for item in d:
 		print "Name: " + d[item][0] + ", Id: " + str(item) + ", Avg: " + str(d[item][1])
 
-print getGrade(1)
-print getGrade(2)
-print getGrade(3)
 print compavg(1)
 print compavg(2)
 print compavg(3)
